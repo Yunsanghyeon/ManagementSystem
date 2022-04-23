bcbc0f3c142a2ab5268a529af4cf00280f08ddc0
yunsanghyeon <tbdpdl6531@naver.com>
2022-04-23 21:28:13 +0900

    MystudyManager default package

---
+++ src/MystudyManager.java
@@ -0,0 +1,101 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import subject.EnglishSubject;
+import subject.Subject;
+import subject.SubjectKind;
+public class MystudyManager {
+	ArrayList<Subject> subjects = new ArrayList<Subject>();
+	Scanner input;
+	MystudyManager(Scanner input){
+		this.input=input;
+	}
+	public void addSubject() {
+		int kind = 0;
+		Subject subject;
+		while(kind != 1 && kind != 2) {
+			System.out.print("Select Grade:\n");
+			System.out.print("1 for 1 Grade\n");
+			System.out.print("2 for 2 Grade\n");
+			System.out.print("Select num for Grade Kind between 1 and 2:\n");
+			kind = input.nextInt();
+			if(kind == 1) {
+				subject = new Subject();
+				subject.getSubjectInput(input);
+				subjects.add(subject);
+				break;
+			}
+			else {
+				System.out.print("Select num for Grade Kind:\n");
+			}
+		}
+	}
+	public void deleteSubject(){
+		System.out.print("Subject difficulty:");
+		int subjectdifficulty=input.nextInt();
+		int index = -1;
+		for(int i = 0; i<subjects.size(); i++) {
+			if(subjects.get(i).getdifficulty()==subjectdifficulty) {
+				index = i;
+				break;
+			}
+		}
+		if(index>=0) {
+			subjects.remove(index);
+			System.out.println("the Subject"+subjectdifficulty+"is deleted");
+		}
+		else {
+			System.out.println("the subject has not been registered");
+			return ;
+		}
+	}
+	public void editSubject() {
+		System.out.print("Subject Name:");
+		int subjectdifficulty=input.nextInt();
+		for(int i=0; i<subjects.size(); i++) {
+			Subject subject = subjects.get(i);
+			if(subject.getdifficulty()==subjectdifficulty) {
+				int num = -1;
+				while(num!=5) {
+					System.out.println("**Subject Info Edit Menu**");
+					System.out.println("1. Edit Name");
+					System.out.println("2. Edit Professor");
+					System.out.println("3. Edit Midterm");
+					System.out.println("4. Edit Final exam");
+					System.out.println("5. Exit");
+					System.out.println("Select one number between 1~5:");
+					if(num==1) {
+						System.out.print("Subject Name:");
+						String subname = input.next();
+						subject.setSubname(subname);
+					}
+					else if(num==2) {
+						System.out.print("Professor Name:");
+						String proname =  input.next();
+						subject.setProname(proname);
+					}
+					else if(num==3) {
+						System.out.print("Midterm score:");
+						int midScore =  input.nextInt();
+						subject.setMidScore(midScore);
+					}
+					else if(num==4) {
+						System.out.print("Final exam grade score:");
+						int finScore = input.nextInt();
+						subject.setFinScore(finScore);
+					}
+					else {
+						continue;
+					}//if
+				}//while
+			}
+			break;
+		}//if
+	}//for
+	public void viewSubject() {
+		System.out.println("#of registerd Subjects :"+subjects.size());
+		for(int i = 0; i<subjects.size(); i++) {
+			subjects.get(i).printInfo();
+		}
+	}
+
+}

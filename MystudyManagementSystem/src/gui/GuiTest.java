e23369a202a2a6e85f4409c82cc2005ce3e011e5
yunsanghyeon <tbdpdl6531@naver.com>
2022-05-29 11:25:14 +0900

    My StudyManager Gui

--- MystudyManagementSystem/src/gui/GuiTest.java
+++ MystudyManagementSystem/src/gui/GuiTest.java
@@ -5,7 +5,7 @@ public class GuiTest {
 	public static void main(String[] args) {
 		MenuSelection menuselection = new MenuSelection();
 		//SubjectAdder subjectadder = new SubjectAdder();
-		//SubjectViewer studentadder = new SubjectViewer();
+		//SubjectViewer subjectviewer = new SubjectViewer();
 	}
 
 }

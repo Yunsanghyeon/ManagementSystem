ab8376dfcab2bd13a3ae0053623b55e0248fb936
yunsanghyeon <tbdpdl6531@naver.com>
2022-05-29 11:10:51 +0900

    My StudyManager Gui

--- MystudyManagementSystem/src/gui/SpringUtilities.java
+++ MystudyManagementSystem/src/gui/SpringUtilities.java
@@ -1,66 +1,16 @@
-/* Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- *   - Neither the name of Oracle or the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */ 
 package gui;
 import javax.swing.*;
 import javax.swing.SpringLayout;
 import java.awt.*;
-/**
- * A 1.4 file that provides utility methods for
- * creating form- or grid-style layouts with SpringLayout.
- * These utilities are used by several programs, such as
- * SpringBox and SpringCompactGrid.
- */
+
 public class SpringUtilities {
-    /**
-     * A debugging utility that prints to stdout the component's
-     * minimum, preferred, and maximum sizes.
-     */
+
     public static void printSizes(Component c) {
         System.out.println("minimumSize = " + c.getMinimumSize());
         System.out.println("preferredSize = " + c.getPreferredSize());
         System.out.println("maximumSize = " + c.getMaximumSize());
     }
-    /**
-     * Aligns the first <code>rows</code> * <code>cols</code>
-     * components of <code>parent</code> in
-     * a grid. Each component is as big as the maximum
-     * preferred width and height of the components.
-     * The parent is made just big enough to fit them all.
-     *
-     * @param rows number of rows
-     * @param cols number of columns
-     * @param initialX x location to start the grid at
-     * @param initialY y location to start the grid at
-     * @param xPad x padding between cells
-     * @param yPad y padding between cells
-     */
+
     public static void makeGrid(Container parent,
                                 int rows, int cols,
                                 int initialX, int initialY,
@@ -77,8 +27,7 @@ public class SpringUtilities {
         Spring initialXSpring = Spring.constant(initialX);
         Spring initialYSpring = Spring.constant(initialY);
         int max = rows * cols;
-        //Calculate Springs that are the max of the width/height so that all
-        //cells have the same size.
+
         Spring maxWidthSpring = layout.getConstraints(parent.getComponent(0)).
                                     getWidth();
         Spring maxHeightSpring = layout.getConstraints(parent.getComponent(0)).
@@ -89,16 +38,14 @@ public class SpringUtilities {
             maxWidthSpring = Spring.max(maxWidthSpring, cons.getWidth());
             maxHeightSpring = Spring.max(maxHeightSpring, cons.getHeight());
         }
-        //Apply the new width/height Spring. This forces all the
-        //components to have the same size.
+
         for (int i = 0; i < max; i++) {
             SpringLayout.Constraints cons = layout.getConstraints(
                                             parent.getComponent(i));
             cons.setWidth(maxWidthSpring);
             cons.setHeight(maxHeightSpring);
         }
-        //Then adjust the x/y constraints of all the cells so that they
-        //are aligned in a grid.
+
         SpringLayout.Constraints lastCons = null;
         SpringLayout.Constraints lastRowCons = null;
         for (int i = 0; i < max; i++) {

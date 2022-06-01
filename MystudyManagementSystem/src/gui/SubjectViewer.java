d2793d337ef703312ff71dbbc5b6a4b4fbea1793
yunsanghyeon <tbdpdl6531@naver.com>
2022-06-01 16:14:01 +0900

    My Xmanager mouseevent

--- MystudyManagementSystem/src/gui/SubjectViewer.java
+++ MystudyManagementSystem/src/gui/SubjectViewer.java
@@ -1,23 +1,30 @@
 package gui;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-public class SubjectViewer extends JFrame{
-	public SubjectViewer() {
-		DefaultTableModel model = new DefaultTableModel();
-		model.addColumn("CORD NUMBER");
-		model.addColumn("BOOK NAME");
-		model.addColumn("PROFESSOR NAME");
-		model.addColumn("SCORE");
-		JTable table = new JTable(model);
-		JScrollPane sp = new JScrollPane(table);
-		this.add(sp);
-		this.setSize(300, 300);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setVisible(true);
-	}
+import MystudyManager;
+
+public class SubjectViewer extends JPanel{
+   WindowFrame frame;
+   MystudyManager mystudyManager;
+   public SubjectViewer(WindowFrame frame, MystudyManager mystudyManager) {
+      this.frame = frame;
+      this.mystudyManager = mystudyManager;
+      
+      System.out.println("***"+mystudyManager.size()+"***");
+      DefaultTableModel model = new DefaultTableModel();
+      model.addColumn("CORD NUMBER");
+      model.addColumn("BOOK NAME");
+      model.addColumn("PROFESSOR NAME");
+      model.addColumn("SCORE");
+      JTable table = new JTable(model);
+      JScrollPane sp = new JScrollPane(table);
+      this.add(sp);
+
+   }
 
 }
---
+++ MystudyManagementSystem/src/gui/WindowFrame.java
@@ -0,0 +1,49 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import MystudyManager;
+
+public class WindowFrame extends JFrame{
+   MystudyManager mystudyManager;
+   MenuSelection menuselection;
+   SubjectAdder subjectadder;
+   SubjectViewer subjectviewer;
+   public WindowFrame(MystudyManager mystudyManager) {
+      this.setSize(500,300);
+      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//x누르면 꺼진다.
+      this.mystudyManager = mystudyManager;
+      menuselection = new MenuSelection(this);
+      subjectadder = new SubjectAdder(this);
+      subjectviewer = new SubjectViewer(this, this.mystudyManager);
+      
+      this.setupPanel(menuselection);
+      this.setVisible(true);
+   }
+   public void setupPanel(JPanel panel) {
+      this.getContentPane().removeAll();
+      this.getContentPane().add(panel);
+      this.revalidate();
+      this.repaint();
+      
+   }
+   public MenuSelection getMenuselection() {
+      return menuselection;
+   }
+   public void setMenuselection(MenuSelection menuselection) {
+      this.menuselection = menuselection;
+   }
+   public SubjectAdder getSubjectadder() {
+      return subjectadder;
+   }
+   public void setSubjectadder(SubjectAdder subjectadder) {
+      this.subjectadder = subjectadder;
+   }
+   public SubjectViewer getSubjectviewer() {
+      return subjectviewer;
+   }
+   public void setSubjectviewer(SubjectViewer subjectviewer) {
+      this.subjectviewer = subjectviewer;
+   }
+}
